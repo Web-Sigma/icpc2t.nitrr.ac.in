8dfec3ca496ea98517782e55f93565d0337ecf45
Anyleo <manjushaanmol@gmail.com>
2021-06-02 20:48:55 +0530

    About raipur added

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from '../views/Contact.vue'
+import Registration  from '../views/Registration.vue'
 import Aboutindia  from '../views/Aboutindia.vue'
 import Aboutraipur  from '../views/Aboutraipur.vue'
-<<<<<<< HEAD
-=======
-import Callpapers  from '../views/Callpapers.vue'
-import ConferenceVenue  from '../views/ConferenceVenue.vue'
-import Downloads  from '../views/Downloads.vue'
-import Event_schedule  from '../views/Event_schedule.vue'
-import Keynote_Speakers  from '../views/Keynote_Speakers.vue'
-import Partners  from '../views/Committee.vue'
-import Workshops  from '../views/Workshops.vue'
-import Registration  from '../views/Registration.vue'
-
->>>>>>> e2d9809b067d94841a1c41923c3270d891ae47b7
 Vue.use(VueRouter)
 
 const routes = [
@@ -37,62 +26,7 @@ const routes = [
   {
     path: '/india',
     name: 'Aboutindia',
-    component:Aboutindia
-  },
-  {
-    path: '/raipur',
-    name: 'Aboutraipur',
-    component:Aboutraipur
-  },
-  {
-    path: '/call-for-papers',
-    name: 'Callpapers',
-    component:Callpapers
-  },
-  // {
-  //   path: '/advisory',
-  //   name: 'Advisory_Committee',
-  //   component:Advisory_Committee
-  // },
-  // {
-  //   path: '/organising',
-  //   name: 'Organising_Committee',
-  //   component:Organising_Committee
-  // },
-  // {
-  //   path: '/technical',
-  //   name: 'Technical_Committee',
-  //   component:Technical_Committee
-  // },
-  {
-    path: '/venue',
-    name: 'ConferenceVenue',
-    component:ConferenceVenue
-  },
-  {
-    path: '/downloads',
-    name: 'Downloads',
-    component:Downloads
-  },
-  {
-    path: '/schedule',
-    name: 'Event_schedule',
-    component:Event_schedule
-  },
-  {
-    path: '/speakers',
-    name: 'Keynote_Speakers',
-    component:Keynote_Speakers
-  },
-  {
-    path: '/partners',
-    name: 'Partners',
-    component:Partners
-  },
-  {
-    path: '/workshops',
-    name: 'Workshops',
-    component:Workshops
+    component: Aboutindia
   },
   {
     path: '/raipur',
