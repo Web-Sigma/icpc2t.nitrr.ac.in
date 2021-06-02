1be3017325197a2e592dfb3cec51099a78e701f2
Anyleo <manjushaanmol@gmail.com>
2021-06-02 21:19:18 +0530

    worksops modified

--- src/views/Workshops.vue
+++ src/views/Workshops.vue
@@ -1,32 +1,33 @@
 <template>
-  <div>
+<div>
 
 
-    <section id="post-96" class="post-96 page type-page status-publish hentry">
-    		<p style="color: #006699;font-size:25px;"><strong>WORKSHOPS</strong></p>
-                            <!-- Swiper -->
-                            <div class="swiper-container swiper-container-initialized swiper-container-horizontal">
-                                <div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
-                                    <div class="swiper-slide swiper-slide-active" style="margin-left: auto; width: 817px;">
-                                      <!-- <img src="./nivo/demo/12.jpg"> -->
-                                    </div>
-                                    <div class="swiper-slide swiper-slide-next" style="width: 817px;">
-                                      <!-- <img src="./nivo/demo/13.jpg"> -->
-                                    </div>
-                                </div>
-                                <!-- Add Arrows -->
-                                <div class="swiper-button-next" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false"></div>
-                                <div class="swiper-button-prev swiper-button-disabled" tabindex="0" role="button" aria-label="Previous slide" aria-disabled="true"></div>
-                            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
-    		</section>
+  <section id="post-96" class="post-96 page type-page status-publish hentry">
+    <p style="color: #006699;font-size:25px;"><strong>WORKSHOPS</strong></p>
+    <!-- Swiper -->
+    <div class="swiper-container swiper-container-initialized swiper-container-horizontal">
+      <div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px);">
+        <div class="swiper-slide swiper-slide-active" style="margin-left: auto; width: 817px;">
+          <!-- <img src="./nivo/demo/12.jpg"> -->
+        </div>
+        <div class="swiper-slide swiper-slide-next" style="width: 817px;">
+          <!-- <img src="./nivo/demo/13.jpg"> -->
+        </div>
+      </div>
+      <!-- Add Arrows -->
+      <div class="swiper-button-next" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false"></div>
+      <div class="swiper-button-prev swiper-button-disabled" tabindex="0" role="button" aria-label="Previous slide" aria-disabled="true"></div>
+      <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+    </div>
+  </section>
 
 
 
-  </div>
+</div>
 </template>
 
 <script>
-  export default{
-name: "Workshops"
-  };
+export default {
+  name: "Workshops"
+};
 </script>
