d8380f542d9cdd11ffb53ca06e24977d6cf5ed13
Jay Prakash <unijay12@gmail.com>
2021-06-02 11:16:27 +0530

    router added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  name: "App",
-  components: {},
-};
-</script>
-
 <style>
-@import "./assets/event/assets/font-awesome/css/font-awesome.min8e83.css";
-@import "./assets/event/style8e83.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
