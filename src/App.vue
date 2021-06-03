1f2a1eda66e365fb77bdcfbc853a0d543923bd94
Anyleo <manjushaanmol@gmail.com>
2021-06-03 15:23:49 +0530

    just backgroud of dropdown

--- src/App.vue
+++ src/App.vue
@@ -156,7 +156,7 @@ export default {
 .dropdown-content {
   display: none;
   position: absolute;
-  background-color: #f9f9f9;
+  background-color:  #006699;
   min-width: 160px;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
