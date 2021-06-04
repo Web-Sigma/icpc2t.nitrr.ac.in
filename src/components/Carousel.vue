4ce6fd0fac8deaa35e7b142db1d23d5879ab7a09
Anyleo <manjushaanmol@gmail.com>
2021-06-04 21:19:36 +0530

    Carousel added with anmation

---
+++ src/components/Carousel.vue
@@ -0,0 +1,41 @@
+<template> 
+<div class="Carousel">
+<slot> </slot>
+
+<a class="prev" @click="prev">&#10094;</a>
+<a class="next" @click="next">&#10095;</a>
+
+ </div>
+</template>
+
+
+<script> 
+export default{
+
+data(){
+    return {
+
+    };
+},
+methods: {
+next(){
+this.$emit('next')
+},
+prev(){
+this.$emit('prev')
+},
+},
+} 
+</script>
+
+<style>
+.prev {
+    position:reative;
+    height:400px;
+}
+.next {
+    position:relative;
+     height:400px;
+   padding-left:600px;
+}
+</style>
