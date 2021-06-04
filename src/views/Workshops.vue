80f2d7180d18cfdda460c40391f8489683aaa3f9
Anyleo <manjushaanmol@gmail.com>
2021-06-05 00:26:37 +0530

    Keynote speakers and Workshop carousel added with functionality

--- src/views/Workshops.vue
+++ src/views/Workshops.vue
@@ -1,54 +1,55 @@
 <template>
   <div>
-        <div id="primary">
-      <div id="main" class="clearfix">
-    <section id="post-96" class="post-96 page type-page status-publish hentry">
-      <p style="color: #006699; font-size: 25px"><strong>WORKSHOPS</strong></p>
-      <!-- Swiper -->
-      <div
-        class="swiper-container swiper-container-initialized swiper-container-horizontal"
+    <carousel id="primary"
+      :per-page="1"
+      :mouse-drag="false"
+      :autoplay=true
+      :speed=2000
+      :loop=true
+      :autoplayHoverPause=true
+    >
+      <slide
+        data-index="0"
+        data-name="MySlideName"
+      
+        :centerMode="true"
       >
-        <div
-          class="swiper-wrapper"
-          style="transform: translate3d(0px, 0px, 0px)"
-        >
-          <div
-            class="swiper-slide swiper-slide-active"
-            style="margin-left: auto; width: 817px"
-          >
-            <!-- <img src="./nivo/demo/12.jpg"> -->
-          </div>
-          <div class="swiper-slide swiper-slide-next" style="width: 817px">
-            <!-- <img src="./nivo/demo/13.jpg"> -->
-          </div>
-        </div>
-        <!-- Add Arrows -->
-        <div
-          class="swiper-button-next"
-          tabindex="0"
-          role="button"
-          aria-label="Next slide"
-          aria-disabled="false"
-        ></div>
-        <div
-          class="swiper-button-prev swiper-button-disabled"
-          tabindex="0"
-          role="button"
-          aria-label="Previous slide"
-          aria-disabled="true"
-        ></div>
-        <span
-          class="swiper-notification"
-          aria-live="assertive"
-          aria-atomic="true"
-        ></span>
-      </div>
-    </section>
-  </div>  </div>  </div>
+        <img class="carousel-img" src="@/assets/Workshop1.jpg" />
+      </slide>
+      <slide
+        data-index="1"
+        data-name="MySlideName"
+      
+        :centerMode="true"
+      >
+        <img class="carousel-img" src="@/assets/workshop3.jpeg" />
+      </slide>
+      <slide
+        data-index="2"
+        data-name="MySlideName"
+      
+        :centerMode="true"
+      >
+        <img class="carousel-img" src="@/assets/Workshop2.jpeg" />
+      </slide>
+      
+    </carousel>
+  </div>
 </template>
-
 <script>
+import { Carousel, Slide } from 'vue-carousel';
 export default {
   name: "Workshops",
+  components:{
+    Carousel, 
+    Slide
+  }
 };
 </script>
+
+<style scoped>
+.carousel-img {
+  height: 800px;
+  width: 100%;
+}
+</style>
