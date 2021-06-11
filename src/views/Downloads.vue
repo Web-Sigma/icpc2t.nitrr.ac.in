4887452d1cef3a12dabcf4dd46bc9c83e28fafd3
Jay Prakash <unijay12@gmail.com>
2021-06-12 02:54:26 +0530

    Downlods done

---
+++ src/views/Downloads.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-xl-8 col-md-12">
+    <!-- Post -->
+    <div class="row mt-2 mb-5 pb-3 mx-2">
+      <!-- Card -->
+      <div class="card card-body mb-5">
+        <h2 class="font-weight-bold mt-3">
+          <strong>Downloads</strong>
+        </h2>
+        <hr class="red title-hr" />
+        <ul class="list-group z-depth-1 mt-4">
+          <li class="list-group-item d-flex ">
+            <a href="/books/a-great-book.pdf" class="red-text" target="_blank">
+              Download ICPC<sup>2</sup>T 2021 - Brochure
+              <i class="fas fa-file-pdf"> </i
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
