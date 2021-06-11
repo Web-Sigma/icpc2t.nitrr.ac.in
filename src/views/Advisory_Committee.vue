cbd32f716b323975362eabec6481ce0493594345
Jay Prakash <unijay12@gmail.com>
2021-06-12 02:32:19 +0530

    advisory committee done

---
+++ src/views/Advisory_Committee.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="col-xl-8 col-md-12">
+    <!-- Post -->
+    <div class="row mt-2 mb-5 pb-3 mx-2">
+      <!-- Card -->
+      <div class="card card-body mb-5">
+        <h2 class="font-weight-bold mt-3">
+          <strong>Advisory Committee</strong>
+        </h2>
+        <hr class="red title-hr" />
+        <p class="article">
+          <span>Prof. Hari Om Gupta, JIIT, Noida ,India </span><br />
+          <span>Prof. D.P. Kothari, VNIT Nagpur, India</span><br />
+          <span>Prof. Vivek Agrawal, I I T Bombay, India</span><br />
+          <span>Prof. R.S. Anand, I I T Roorkee, India</span><br />
+          <span>Prof. Suryanarayana Doolla, I I T Bombay, India</span><br />
+          <span>Prof. Rajendra Prasad, I I T Roorkee, India</span><br />
+          <span>Prof. Biswarup Das, I I T Roorkee, India</span><br />
+          <span
+            >Prof. Z.Y. (Joe) Dong, University of New South Wales,
+            Australia</span
+          ><br />
+          <span
+            >Prof. Almoataz Y. Abdelaziz, Future University of Egypt, Cairo,
+            Egypt</span
+          ><br />
+          <span
+            >Prof. Akshay Kumar Rathore, University of Concordia, Canada</span
+          ><br />
+          <span>Prof. H.M. Suryawanshi,VNIT Nagpur, India</span><br />
+          <span>Prof. R.K. Nema, MANIT Bhopal, India</span><br />
+          <span>Prof. Shailendra Kumar Jain, SLIET, Longowal</span><br />
+          <span>Prof. K. Shanti Swarup, I I T Madras, India</span><br />
+          <span>Prof. Saad Mekhilef, University of Malaya, Malaysia</span><br />
+          <span>Prof. Madhuri Avinash Chaudhari, VNIT Nagpur, India</span><br />
+
+          <span>Prof. Shubhi Purwar, MNNIT Allahabad, India</span><br />
+          <span>Prof. Anupam Shukla, IIIT Pune, India</span><br />
+          <span>Prof. Ritu Tiwari, IIITM Gwalior, India</span><br />
+          <span>Prof. Ashok Kumar Pradhan, I I T Kharagpur, India</span><br />
+          <span>Prof. Anup Kumar Panda, NIT Rourkela, India</span><br />
+          <span>Prof. Tripta Thakur, MANIT Bhopal, India</span><br />
+          <span>Prof. Ketan Detroja, I I T Hyderabad, India</span><br />
+          <span>Prof. Nand Kishor, MNNIT Allahabad, India</span><br />
+          <span
+            >Prof. Abdul Quaiyum Ansari, Jamia Millia Islamia University,
+            India</span
+          ><br />
+        </p>
+      </div>
+      <!-- entry-content clearfix-->
+
+      <div id="comments" class="comments-area"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Advisory_Committee",
+};
+</script>
