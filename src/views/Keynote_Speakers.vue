31a7a996beff134525b7dd150a909ffd9b4b3954
Jay Prakash <unijay12@gmail.com>
2021-06-12 03:07:21 +0530

    Organising Committee done

---
+++ src/views/Keynote_Speakers.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="col-xl-8 col-md-12">
+      <!-- Post -->
+      <div class="row mt-2 mb-5 pb-3 mx-2">
+        <!-- Card -->
+        <div class="card card-body mb-5">
+
+        <h2 class="font-weight-bold mt-3">
+          <strong>Speakers</strong>
+        </h2>
+        <hr class="red title-hr" />
+          <!-- <carousel id="primary"
+      :per-page="1"
+      :mouse-drag="false"
+      :autoplay=true
+      :speed=2000
+      :loop=true
+      :autoplayHoverPause=true
+    >
+      <slide
+        data-index="0"
+        data-name="MySlideName"
+      
+        :centerMode="true"
+      >
+        <img class="carousel-img" src="@/assets/Speaker1.jpeg" />
+      </slide>
+      <slide
+        data-index="1"
+        data-name="MySlideName"
+      
+        :centerMode="true"
+      >
+        <img class="carousel-img" src="@/assets/Speaker2.jpeg" />
+      </slide>
+      <slide
+        data-index="2"
+        data-name="MySlideName"
+      
+        :centerMode="true"
+      >
+        <img class="carousel-img" src="@/assets/Speakeer3.jpeg" />
+      </slide>
+     
+    </carousel> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Keynote_Spaeakers",
+  components: {},
+};
+</script>
+
+<style scoped>
+.carousel-img {
+  height: 800px;
+  width: 100%;
+}
+</style>
---
+++ src/views/Organising_Committee.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="col-xl-8 col-md-12">
+    <!-- Post -->
+    <div class="row mt-2 mb-5 pb-3 mx-2">
+      <!-- Card -->
+      <div class="card card-body mb-5">
+        <h2 class="font-weight-bold mt-3">
+          <strong>Organising Committee</strong>
+        </h2>
+        <hr class="red title-hr" />
+        <div class="content">
+          <p class="article">
+            <span class="heading">Chief Patron(s)</span><br />
+            <span
+              >Dr. A. M. Rawani, Director, National Institute of Technology
+              Raipur</span
+            ><br />
+            <span class="heading">Patron(s)</span><br />
+            <span
+              >Dr. Shubhrata Gupta, Dean (Research &amp; Consultancy), National
+              Institute of Technology Raipur</span
+            ><br /><span>
+              Dr. Shrish Verma, Dean (Academics), National Institute of
+              Technology Raipur</span
+            ><br />
+
+            <span class="heading">General Chairs</span><br />
+            <span
+              >Dr. Narendra D. Londhe, National Institute of Technology
+              Raipur</span
+            ><br />
+            <span class="heading">Organising Secretaries</span><br />
+            <span
+              >Dr. Ram Narayan Patel, National Institute of Technology
+              Raipur</span
+            ><br />
+            <span
+              >Dr. Anamika Yadav, National Institute of Technology Raipur</span
+            ><br />
+            <span
+              >Dr. Subhojit Ghosh, National Institute of Technology Raipur</span
+            ><br />
+            <span class="heading">Technical Program Chairs</span><br />
+            <span
+              >Dr. P. D. Dewangan, National Institute of Technology Raipur</span
+            ><br />
+            <span>Dr. Sachin Jain, National Institute of Technology Raipur</span
+            ><br />
+            <span class="heading">Publication Chairs</span><br />
+            <span>Dr. Ebha Koley, National Institute of Technology Raipur</span
+            ><br />
+            <span
+              >Dr. Swapnajit Pattnaik, National Institute of Technology
+              Raipur</span
+            ><br />
+            <span class="heading">Publicity Chairs</span><br />
+            <span
+              >Dr. Varsha Singh, National Institute of Technology Raipur</span
+            ><br />
+            <span
+              >Dr. Monalisa Biswal, National Institute of Technology
+              Raipur</span
+            ><br />
+            <span class="heading">Finance Chair</span><br />
+            <span>Dr. Lalit Kumar, National Institute of Technology Raipur</span
+            ><br />
+            <span
+              >Dr. Baidyanath Bag, National Institute of Technology Raipur</span
+            ><br />
+            <span class="heading">Track Chairs</span><br />
+            <span>Dr. B. Shaw, National Institute of Technology Raipur</span
+            ><br />
+            <span
+              >Dr. K. Chandrasekaran, National Institute of Technology
+              Raipur</span
+            ><br />
+            <span class="heading">Student Branch Chairs</span><br />
+            <span
+              >Shreya Chandrakar, National Institute of Technology Raipur</span
+            ><br />
+            <span
+              >Kartikay Shankar, National Institute of Technology Raipur</span
+            ><br />
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Organising_Committee",
+};
+</script>
+<style scoped>
+.heading {
+  color: #006699;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: underline;
+  padding-bottom: 5px;
+}
+</style>
