cc0c91a2c8e4242d4c39efd80cc9fe333499ad09
Jay Prakash <unijay12@gmail.com>
2021-06-12 02:49:03 +0530

    Contact us done

---
+++ src/views/Contact.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-xl-8 col-md-12">
+    <!-- Post -->
+    <div class="row mt-2 mb-5 pb-3 mx-2">
+      <!-- Card -->
+      <div class="card card-body mb-5">
+        <h2 class="font-weight-bold mt-3">
+          <strong>Contact Us</strong>
+        </h2>
+        <hr class="red title-hr" />
+
+        <div class="entry-content" itemprop="text">
+          <p class="article">
+            <span style=""
+              >Please contact for further details and clarifications:</span
+            >
+          </p>
+          <p class="article">
+            <span style="font-size: 12pt"><strong>Dr. R N Patel</strong></span
+            ><br />
+            <span style="font-size: 12pt">Organizing Chair,</span><br />
+            <span style="font-size: 12pt">ICPC<sup>2</sup>T 2020,</span><br />
+            <span style="font-size: 12pt">National Institute of Technology</span
+            ><br />
+            <span style="font-size: 12pt">
+              Raipur, Chhattisgarh - 492010, India</span
+            ><br />
+
+            <span style="font-size: 12pt"
+              >E-mail: <a href="mailto:icpc2t@nitrr.ac.in">icpc2t@nitrr.ac.in</a></span
+            ><br />
+
+            <span style="font-size: 12pt"><br /> </span>
+          </p>
+        </div>
+        <!-- entry-content clearfix-->
+
+        <div id="comments" class="comments-area"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
