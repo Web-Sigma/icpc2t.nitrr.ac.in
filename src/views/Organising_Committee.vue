194cb2323c566a0ea4728f2d733ab08d45abe47b
Jay Prakash <unijay12@gmail.com>
2021-06-14 15:20:06 +0530

    Organising Committee updated

--- src/views/Organising_Committee.vue
+++ src/views/Organising_Committee.vue
@@ -10,77 +10,55 @@
         <hr class="red title-hr" />
         <div class="content">
           <p class="article">
-            <span class="heading">Chief Patron(s)</span><br />
+            <br /><span class="heading">Chief Patron(s)</span><br />
             <span
-              >Dr. A. M. Rawani, Director, National Institute of Technology
-              Raipur</span
+              >Dr. A. M. Rawani, Director, NIT Raipur</span
             ><br />
-            <span class="heading">Patron(s)</span><br />
+            <br /><span class="heading">Patron(s)</span><br />
             <span
-              >Dr. Shubhrata Gupta, Dean (Research &amp; Consultancy), National
-              Institute of Technology Raipur</span
+              >Dr. Shubhrata Gupta, Dean (Research &amp; Consultancy), NIT Raipur</span
             ><br /><span>
-              Dr. Shrish Verma, Dean (Academics), National Institute of
-              Technology Raipur</span
+              Dr. Shrish Verma, Dean (Academics), NIT Raipur</span
             ><br />
+            <br /><span class="heading">Honorary Chair</span><br />
+            <span
+              >Prof. Frede Blaabjerg, Professor, Villum Investigator, Aalborg University, Denmark</span
+            ><br />
+            <span
+              >Prof. Vivek Agarwal, Institute Chair Professor, EED,
+              IIT-Bombay</span
+            ><br />
+            <span
+              >Prof. Suryanarayana Doolla, Professor, Dept. of ESE, IIT
+              Bombay</span
+            ><br />
+            <br /><span class="heading">General Chairs</span><br />
+            <span
+              >Dr. Narendra D. Londhe, NIT Raipur</span
+            ><br />
+            <br /><span class="heading">Coordinators/ Organizing Secretaries</span
+            ><br />
+            <span>Dr. Sachin Jain, NIT Raipur </span><br />
+            <span>Dr. Swapnajit Patnaik, NIT Raipur </span><br />
+            <span>
+              Dr. Ebha Koley, NIT Raipur </span
+            ><br />
+            <br /><span class="heading">Publication Chairs</span><br />
+            <span>Prof. P. D. Dewangan, NIT Raipur</span><br />
+            <span>Dr. S. Ghosh, NIT Raipur</span><br />
+            <span>Dr. R.N. Patel, NIT Raipur</span><br />
+            <span>Dr. Pradyumn Chaturvedi, VNIT Nagpur</span><br />
+            <br /><span class="heading">Publicity Chairs</span><br />
+            <span>Dr. Anamika Jain, NIT Raipur</span><br />
+            <span>Dr. Ritesh Keshri, VNIT Nagpur</span><br /><span
+              >Dr. (Mrs) Varsha Singh, NIT Raipur</span
+            ><br />
+            <span>Dr. (Mrs) Monalisa Biswal, NIT Raipur</span><br />
 
-            <span class="heading">General Chairs</span><br />
-            <span
-              >Dr. Narendra D. Londhe, National Institute of Technology
-              Raipur</span
-            ><br />
-            <span class="heading">Organising Secretaries</span><br />
-            <span
-              >Dr. Ram Narayan Patel, National Institute of Technology
-              Raipur</span
-            ><br />
-            <span
-              >Dr. Anamika Yadav, National Institute of Technology Raipur</span
-            ><br />
-            <span
-              >Dr. Subhojit Ghosh, National Institute of Technology Raipur</span
-            ><br />
-            <span class="heading">Technical Program Chairs</span><br />
-            <span
-              >Dr. P. D. Dewangan, National Institute of Technology Raipur</span
-            ><br />
-            <span>Dr. Sachin Jain, National Institute of Technology Raipur</span
-            ><br />
-            <span class="heading">Publication Chairs</span><br />
-            <span>Dr. Ebha Koley, National Institute of Technology Raipur</span
-            ><br />
-            <span
-              >Dr. Swapnajit Pattnaik, National Institute of Technology
-              Raipur</span
-            ><br />
-            <span class="heading">Publicity Chairs</span><br />
-            <span
-              >Dr. Varsha Singh, National Institute of Technology Raipur</span
-            ><br />
-            <span
-              >Dr. Monalisa Biswal, National Institute of Technology
-              Raipur</span
-            ><br />
-            <span class="heading">Finance Chair</span><br />
-            <span>Dr. Lalit Kumar, National Institute of Technology Raipur</span
-            ><br />
-            <span
-              >Dr. Baidyanath Bag, National Institute of Technology Raipur</span
-            ><br />
-            <span class="heading">Track Chairs</span><br />
-            <span>Dr. B. Shaw, National Institute of Technology Raipur</span
-            ><br />
-            <span
-              >Dr. K. Chandrasekaran, National Institute of Technology
-              Raipur</span
-            ><br />
-            <span class="heading">Student Branch Chairs</span><br />
-            <span
-              >Shreya Chandrakar, National Institute of Technology Raipur</span
-            ><br />
-            <span
-              >Kartikay Shankar, National Institute of Technology Raipur</span
-            ><br />
+            <br /><span class="heading">Finance Chair</span><br />
+            <span>Dr. B. Shaw, NIT Raipur </span><br />
+            <span>Dr Lalit Kumar, NIT Raipur</span><br />
+            <span>Dr. Baidyanath Bag, NIT Raipur</span><br />
           </p>
         </div>
       </div>
