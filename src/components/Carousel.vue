012eb017a950a51e9d0be7d1f02bfbd1a5ce01e0
Anyleo <manjushaanmol@gmail.com>
2021-06-24 12:58:52 +0530

    Carousel updated

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,4 +1,5 @@
 <template>
+<div id="carousel-main-div-for-spacing">
   <!-- Carousel Wrapper -->
   <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
 
@@ -10,18 +11,117 @@
            
           >
             <slide>
-              <img src="@/assets/nit-entrance.jpg" alt="" />
+             
+        <!--First slide-->
+        <div class="carousel-item active">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/GateNIT.jpg"
+                  alt="Card image cap">
+                
+              </div>
+            </div>
+
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/NIT.jpg"
+                  alt="Card image cap">
+              
+              </div>
+            </div>
+
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/nit-entrance.jpg"
+                  alt="Card image cap">
+                
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!--/.First slide-->
+
             </slide>
             <slide>
-              <img src="@/assets/banner.jpeg" alt="" /> </slide
+              
+        <!--Second slide-->
+        <div class="carousel-item active">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/nit-entrance.jpg"
+                  alt="Card image cap">
+                
+              </div>
+            </div>
+
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/nitfront.jpg"
+                  alt="Card image cap">
+               
+              </div>
+            </div>
+
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/NIT.jpg"
+                  alt="Card image cap">
+               
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!--/.Second slide-->
+ </slide
             ><slide>
-              <img src="@/assets/gadi-chowk.jpg" alt="" />
+             
+        <!--Third slide-->
+        <div class="carousel-item active">
+
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/nitnight.jpg"
+                  alt="Card image cap">
+                
+              </div>
+            </div>
+
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/NIT.jpg"
+                  alt="Card image cap">
+                
+              </div>
+            </div>
+
+            <div class="col-md-4 clearfix d-none d-md-block">
+              <div class="card mb-2">
+                <img class="card-img-top" src="@/assets/nitnight gate.jpg"
+                  alt="Card image cap">
+                
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!--/.Third slide-->
+
             </slide>
           </carousel>
           
   </div>
   <!-- Carousel Wrapper -->
 
+  
+
+</div>
   
 </template>
 
@@ -32,6 +132,16 @@ export default {
 </script>
 
 <style scoped>
+#carousel-main-div-for-spacing{
+  margin-right: 50px;
+  margin-left: 50px;
+}
+.col-md-4{
+padding-top: 0;
+padding-right: 5px;
+padding-bottom: 0;
+padding-left: 5px;
+}
 img {
   max-height:220px;
   height: 100%;
