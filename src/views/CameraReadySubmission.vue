fae220c0e89bbd26f708c0ab67956b1d4fa30878
Anyleo <manjushaanmol@gmail.com>
2021-06-24 18:03:56 +0530

    downloads brochere updated

--- src/views/CameraReadySubmission.vue
+++ src/views/CameraReadySubmission.vue
@@ -1,45 +1,103 @@
 <template>
-    <div>
-         <div class="col-xl-8 col-md-12">
-    <!-- Post -->
-    <div class="row mt-2 mb-5 pb-3 mx-2">
-      <!-- Card -->
-      <div class="card card-body mb-5">
-        <h2 class="font-weight-bold mt-3">
-          <strong>Camera Ready Submission Instructions </strong>
-        </h2>
-        <hr class="red title-hr" />
+  <div>
+    <div class="col-xl-8 col-md-12">
+      <!-- Post -->
+      <div class="row mt-2 mb-5 pb-3 mx-2">
+        <!-- Card -->
+        <div class="card card-body mb-5">
+          <h2 class="font-weight-bold mt-3">
+            <strong>Camera Ready Submission Instructions </strong>
+          </h2>
+          <hr class="red title-hr" />
 
-        <p> Please follow the instructions given below for submitting the final camera-ready paper: </p>
-        <h2> Step 1: Final Camera-ready paper preparation </h2>
-<p> (a) Please follow the IEEE two-column A4 size standard conference format for preparing your final manuscript also. For more details please visit: IEEE Templates page</p>
-<a> https://www.ieee.org/conferences/publishing/templates.html </a>
-<p>(b) Revised Version: Revise your paper based on reviewers’ comments/suggestions (if any). The reviewer’s comments are available in the same Microsoft paper management site </p>
-<a>https://cmt3.research.microsoft.com/IPRECON2021 </a>
-<p> 
-(c) The maximum page limit is six pages including references. No author biography at the end. In case additional pages are necessary, pages up to 8 pages are permitted subject to payment of Rs. 500 ($10) extra per each additional page.
+          <p>
+            Please follow the instructions given below for submitting the final
+            camera-ready paper:
+          </p>
+          <h2>Step 1: Final Camera-ready paper preparation</h2>
+          <p>
+            (a) Please follow the IEEE two-column A4 size standard conference
+            format for preparing your final manuscript also. For more details
+            please visit: IEEE Templates page
+          </p>
+          <a> https://www.ieee.org/conferences/publishing/templates.html </a>
+          <p>
+            (b) Revised Version: Revise your paper based on reviewers’
+            comments/suggestions (if any). The reviewer’s comments are available
+            in the same Microsoft paper management site
+          </p>
+          <a>https://cmt3.research.microsoft.com/IPRECON2021 </a>
+          <p>
+            (c) The maximum page limit is six pages including references. No
+            author biography at the end. In case additional pages are necessary,
+            pages up to 8 pages are permitted subject to payment of Rs. 500
+            ($10) extra per each additional page. (d) Similarity with other
+            papers should be low (Generally, a similarity index of more than 30
+            % is not acceptable for publication). Maximum File Size allowed is 3
+            MB and the file format is Adobe PDF (.pdf) without any encryption
+            and/or passwords. (e) The authors are expected to make sure that the
+            comments of the reviewers/meta-reviewers have been addressed in the
+            final manuscript. Reply to the reviewer/meta reviewer comments can
+            be given in the comment column in the ‘post author feedback’ in
+            author console in CMT. Further, a separate pdf file highlighting the
+            changes should be uploaded as the author's feedback file on the same
+            page.
+          </p>
+          <h2>Step 2. PDF eXpress check:</h2>
+          <p>
+            After you have appropriately revised your paper, pass your revised
+            paper through PDF eXpress using conference ID (52453X) by going to
+            the URL <a> (https://ieee-pdf-express.org)</a>. If you do not have
+            an account in Pdf eXpress, click on the link “New Users – Click
+            Here” and fill in your information (email address and password). If
+            you are not familiar with Pdf eXpress you can get detailed
+            instructions on using IEEE PDF eXpress at:
+          </p>
+          <a> https://ieee-pdf-express.org/External/UsingIEEEPDFeXpress </a>
+          <h2>Step 3. Paper Submission:</h2>
+          <p>
+            Submit the final version of the paper (after passing it through PDF
+            express check) through the author console in CMT. Please change the
+            file name of the pdf Express checked camera-ready paper to PIDxx
+            (where xx is your paper ID in CMT) before submission to CMT. Please
+            note that authors are expected to retain the title of the original
+            paper in the final paper. Also, the author list in the original
+            paper can not be changed in the final manuscript.
+          </p>
+          <h2>Step 4. Submission of the copyright form:</h2>
+          <p>
+            After uploading the final camera-ready paper (the paper must have
+            passed PDF Check-in IEEE PDF eXpress), click on the ‘Submit IEEE
+            Copyright Form’ link to enter the IEEE Copyright Form Submission CMT
+            page. Click on the IEEE copyright website link. Now, the author will
+            be redirected to the IEEE eCF site to submit copyright form. After
+            filling out the IEEE copyright form and copyright agreements, there
+            is an option to download the completed IEEE Copyright Form in PDF
+            (The corresponding author would have received the copyright form
+            through email also). Please upload it (or ‘drop files here’ menu) on
+            the same page in CMT. Please note that the corresponding author can
+            submit the copyright form for all the authors.
+          </p>
+          <h2>Step 5: Conference registration</h2>
+          <p>
+            A full conference registration is mandatory to have the paper
+            included in the conference program and the proceedings. The
+            registration portal will be opened soon. Please check the website
+            regularly for updates.
+          </p>
+          <h2>No Show Policy :</h2>
+          <p>
+            Each accepted paper included in the final program is expected to
+            have at least one author attend and present the paper at the
+            conference. Non-presented paper will not be submitted to IEEE
+            Xplore® digital library as per IEEE no-show policy.
+          </p>
 
-(d) Similarity with other papers should be low (Generally, a similarity index of more than 30 % is not acceptable for publication). Maximum File Size allowed is 3 MB and the file format is Adobe PDF (.pdf) without any encryption and/or passwords.
-
-(e) The authors are expected to make sure that the comments of the reviewers/meta-reviewers have been addressed in the final manuscript. Reply to the reviewer/meta reviewer comments can be given in the comment column in the ‘post author feedback’ in author console in CMT. Further, a separate pdf file highlighting the changes should be uploaded as the author's feedback file on the same page. </p>
- <h2> Step 2. PDF eXpress check:</h2>
-<p> After you have appropriately revised your paper, pass your revised paper through PDF eXpress using conference ID (52453X) by going to the URL <a> (https://ieee-pdf-express.org)</a>. If you do not have an account in Pdf eXpress, click on the link “New Users – Click Here” and fill in your information (email address and password). If you are not familiar with Pdf eXpress you can get detailed instructions on using IEEE PDF eXpress at: </p>
- <a>  https://ieee-pdf-express.org/External/UsingIEEEPDFeXpress </a>
- <h2> Step 3. Paper Submission:</h2>
- <p> Submit the final version of the paper (after passing it through PDF express check) through the author console in CMT. Please change the file name of the pdf Express checked camera-ready paper to PIDxx (where xx is your paper ID in CMT) before submission to CMT. Please note that authors are expected to retain the title of the original paper in the final paper. Also, the author list in the original paper can not be changed in the final manuscript. </p>
- <h2> Step 4. Submission of the copyright form:</h2>
-  <p> After uploading the final camera-ready paper (the paper must have passed PDF Check-in IEEE PDF eXpress), click on the ‘Submit IEEE Copyright Form’ link to enter the IEEE Copyright Form Submission CMT page. Click on the IEEE copyright website link. Now, the author will be redirected to the IEEE eCF site to submit copyright form. After filling out the IEEE copyright form and copyright agreements, there is an option to download the completed IEEE Copyright Form in PDF (The corresponding author would have received the copyright form through email also). Please upload it (or ‘drop files here’ menu) on the same page in CMT. Please note that the corresponding author can submit the copyright form for all the authors.</p>
- <h2>Step 5: Conference registration</h2>
-  <p> A full conference registration is mandatory to have the paper included in the conference program and the proceedings. The registration portal will be opened soon. Please check the website regularly for updates.</p>
- <h2> No Show Policy : </h2>
- <p> Each accepted paper included in the final program is expected to have at least one author attend and present the paper at the conference. Non-presented paper will not be submitted to IEEE Xplore® digital library as per IEEE no-show policy. </p>
-
-        <!-- <p style="text-align: justify"> </p> -->
+          <!-- <p style="text-align: justify"> </p> -->
+        </div>
       </div>
     </div>
   </div>
-        
-        </div>
 </template>
 
 
@@ -49,8 +107,7 @@ export default {
 };
 </script>
 <style scoped>
-p{
-    text-align: justify;
-    }
-
+p {
+  text-align: justify;
+}
 </style>
