1fd3264c4902ff0fae01a2dc15d262bd9e9bcbf8
Jay Prakash <unijay12@gmail.com>
2021-06-25 00:21:18 +0530

    late night commit

--- src/views/Advisory_Committee.vue
+++ src/views/Advisory_Committee.vue
@@ -17,16 +17,19 @@
           <span>Prof. G.K. Singh, IIT Roorkee.</span><br />
           <span> Prof. C. Nagamani, NIT Trichy,</span><br />
           <span>Prof. Rajesh Kumar, MNIT, Jaipur</span><br />
-          ><br />
+          <br />
           <span> Dr. Santanu Kapat, IIT Kharagpur</span><br />
           <span> Dr. Sandeep Anand, IIT-Bombay</span><br />
           <span> Er. Sanjeev Jain, CE, CREDA, Chhattisgarh</span><br />
           <span> Dr. Dheeraj K. Khatod, IIT Roorkee</span><br />
-        <br />
+          <br />
           <span>
             Dr. Rahman Dashti, Persian Gulf University, Bushehr, Iran.</span
           ><br />
-           <span> Prof. Ramesh Bansal, University of Sharjah, United Arab Emirates</span><br />
+          <span>
+            Prof. Ramesh Bansal, University of Sharjah, United Arab
+            Emirates</span
+          ><br />
         </p>
       </div>
       <!-- entry-content clearfix-->
